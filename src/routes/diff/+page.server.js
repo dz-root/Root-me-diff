f670522425338f14bf98d37bbc54494b4bb38693
dz-root <dz-root@hotmail.fr>
2024-01-23 00:09:26 +0100

    Refactoring - Do parsing from server side.

--- src/routes/diff/+page.server.js
+++ src/routes/diff/+page.server.js
@@ -1,7 +1,30 @@
 import axios from 'axios'
+import cheerio from 'cheerio'
 import { redirect } from '@sveltejs/kit'
+import { RankIncon } from '$lib/utils/UserRankIcon.js'
+import OrderByCategory from '$lib/utils/OrderByCategory.js'
+
+let challenges = [], challenges_2 = []
+let groupedByCategory  = [], groupedByCategory_2 = []
+
+let profile_user_1 = {
+    logo : '',
+    nickname : '',
+    stat: { rank: null, score:null, chall_done:null },
+    rank_icon : '',
+    rank_title : '',
+}
+
+let profile_user_2 = {
+    logo : '',
+    nickname : '',
+    stat: { rank: null, score:null, chall_done:null },
+    rank_icon : '',
+    rank_title : '',
+}
 
 export const load = async ({ request, url, setHeaders }) => {
+
     setHeaders({
         'cache-control': 'max-age=86400',
 	})
@@ -17,13 +40,75 @@ export const load = async ({ request, url, setHeaders }) => {
 
         const content_user_1 = await axios.get(`https://www.root-me.org/${user_1}?inc=score&lang=en`).then(response => response.data).catch(e=>redirect(500, '/'))
         const content_user_2 = await axios.get(`https://www.root-me.org/${user_2}?inc=score&lang=en`).then(response => response.data).catch(e=>redirect(500, '/'))
-  
-        return { content_user_1,  content_user_2}
+
+        const ch_1 = cheerio.load(content_user_1), ch_2 = cheerio.load(content_user_2)
+        const title_l = ch_1("title"), title_2 = ch_2("title")
+
+        if( title_l.text().trim().substring(0,10) !== "Erreur 404" ){
+            const links_1 = ch_1("a.vert, a.rouge");
+            const logo = ch_1(".logo_auteur");
+            const stats = ch_1("h3");
+            const rank_1 = stats.eq(3).text().trim().split(" ").pop();
+            const score_1 = stats.eq(4).text().trim().split(" ").pop();
+            const chall_done = stats.eq(5).text().trim().split(" ").pop();
+        
+            profile_user_1.stat.rank = rank_1;
+            profile_user_1.stat.score = score_1;
+            profile_user_1.stat.chall_done = chall_done;
+            profile_user_1.logo = (logo[0].attribs.src).split('?')[0];
+            profile_user_1.nickname = logo[0].attribs.alt;
+            profile_user_1.rank_icon = RankIncon(score_1);
+                
+        
+            links_1.each((i, link) => {
+                const category = link.attribs.href.split("/")[2];
+                const name = cheerio(link).text().split("\xa0")[1];
+                const points = link.attribs.title.split(" ")[0];
+                const userDone = link.attribs.class === ' vert';
+        
+                challenges.push({ category, name, points, userDone });
+            });
+            
+            groupedByCategory = OrderByCategory(challenges)
+                
+        }else{
+            redirect(307, '/')
+        }
+
+        if( title_2.text().trim().substring(0,10) !== "Erreur 404" ){
+            const links_2 = ch_2("a.vert, a.rouge");
+            const logo_2 = ch_2(".logo_auteur");
+            const stats_2 = ch_2("h3");
+            const rank_2 = stats_2.eq(3).text().trim().split(" ").pop();
+            const score_2 = stats_2.eq(4).text().trim().split(" ").pop();
+            const chall_done_2 = stats_2.eq(5).text().trim().split(" ").pop();
+        
+            profile_user_2.stat.rank = rank_2;
+            profile_user_2.stat.score = score_2;
+            profile_user_2.stat.chall_done = chall_done_2;
+            profile_user_2.logo = (logo_2[0].attribs.src).split('?')[0];
+            profile_user_2.nickname = logo_2[0].attribs.alt;
+            profile_user_2.rank_icon = RankIncon(score_2);
+                
+            links_2.each((i, link) => {
+                const category = link.attribs.href.split("/")[2];
+                const name = cheerio(link).text().split("\xa0")[1];
+                const points = link.attribs.title.split(" ")[0];
+                const userDone = link.attribs.class === ' vert';
+        
+                challenges_2.push({ category, name, points, userDone });
+            });
+        
+            groupedByCategory_2 = OrderByCategory(challenges_2)
+        
+        }else{
+            redirect(307, '/')
+        }
+
+        return { profile_user_1, profile_user_2,  groupedByCategory,  groupedByCategory_2}
 
     }else{
-
         redirect(307, '/')
-
     }
     
 }
--- src/routes/diff/+page.svelte
+++ src/routes/diff/+page.svelte
@@ -1,105 +1,16 @@
 <script>
-    import { goto } from '$app/navigation'
-    import cheerio from 'cheerio'
     import Popupbattle from '$lib/components/Popupbattle.svelte'
-    import OrderByCategory from '$lib/utils/OrderByCategory.js'
     import CategoryMenu from '$lib/components/CategoryMenu.svelte'
-    import {RankIncon} from '$lib/utils/UserRankIcon.js'
 
     export let data
 
-    const html_1 =  data.content_user_1, html_2 =  data.content_user_2
-    
-    $: challenges = []
-    $: challenges_2 = []
-
-    $: groupedByCategory  = []
-    $: groupedByCategory_2 = []
-
-    $: profile_user_1 = {
-        logo : '',
-        nickname : '',
-        stat: { rank: null, score:null, chall_done:null },
-        rank_icon : '',
-        rank_title : '',
-    }
-
-    $: profile_user_2 = {
-        logo : '',
-        nickname : '',
-        stat: { rank: null, score:null, chall_done:null },
-        rank_icon : '',
-        rank_title : '',
-    }
-
-    async function parser(){
-        const ch_1 = cheerio.load(html_1), ch_2 = cheerio.load(html_2)
-        const title_l = ch_1("title"), title_2 = ch_2("title")
-
-        if( title_l.text().trim().substring(0,10) !== "Erreur 404" ){
-            const links_1 = ch_1("a.vert, a.rouge");
-            const logo = ch_1(".logo_auteur");
-            const stats = ch_1("h3");
-            const rank_1 = stats.eq(3).text().trim().split(" ").pop();
-            const score_1 = stats.eq(4).text().trim().split(" ").pop();
-            const chall_done = stats.eq(5).text().trim().split(" ").pop();
-
-            profile_user_1.stat.rank = rank_1;
-            profile_user_1.stat.score = score_1;
-            profile_user_1.stat.chall_done = chall_done;
-            profile_user_1.logo = (logo[0].attribs.src).split('?')[0];
-            profile_user_1.nickname = logo[0].attribs.alt;
-            profile_user_1.rank_icon = RankIncon(score_1);
-            
-
-            links_1.each((i, link) => {
-                const category = link.attribs.href.split("/")[2];
-                const name = cheerio(link).text().split("\xa0")[1];
-                const points = link.attribs.title.split(" ")[0];
-                const userDone = link.attribs.class === ' vert';
-        
-                challenges.push({ category, name, points, userDone });
-            });
-        
-            groupedByCategory = OrderByCategory(challenges)
-            
-        }else{
-            goto( '/');
-        }
-
-        if( title_2.text().trim().substring(0,10) !== "Erreur 404" ){
-            const links_2 = ch_2("a.vert, a.rouge");
-            const logo_2 = ch_2(".logo_auteur");
-            const stats_2 = ch_2("h3");
-            const rank_2 = stats_2.eq(3).text().trim().split(" ").pop();
-            const score_2 = stats_2.eq(4).text().trim().split(" ").pop();
-            const chall_done_2 = stats_2.eq(5).text().trim().split(" ").pop();
-
-            profile_user_2.stat.rank = rank_2;
-            profile_user_2.stat.score = score_2;
-            profile_user_2.stat.chall_done = chall_done_2;
-            profile_user_2.logo = (logo_2[0].attribs.src).split('?')[0];
-            profile_user_2.nickname = logo_2[0].attribs.alt;
-            profile_user_2.rank_icon = RankIncon(score_2);
-            
-            links_2.each((i, link) => {
-                const category = link.attribs.href.split("/")[2];
-                const name = cheerio(link).text().split("\xa0")[1];
-                const points = link.attribs.title.split(" ")[0];
-                const userDone = link.attribs.class === ' vert';
-        
-                challenges_2.push({ category, name, points, userDone });
-            });
-
-            groupedByCategory_2 = OrderByCategory(challenges_2)
-
-        } else {
-            goto( '/');
-        }
-    }
+    let groupedByCategory  = data.groupedByCategory
+    let groupedByCategory_2 = data.groupedByCategory_2
+    let profile_user_1 = data.profile_user_1
+    let profile_user_2 = data.profile_user_2
 </script>
 
-{#await parser()}
+{#await data}
     <div class="flex items-center justify-center">
         <div class="flex flex-col items-center justify-center">
             <img src="./logo.png" alt="loader" class="animate-pulse">
@@ -107,7 +18,8 @@
     </div>
 {:then data}
 
-    <Popupbattle profile_user_1="{profile_user_1}" profile_user_2="{profile_user_2}"/>
+    <Popupbattle profile_user_1="{profile_user_1}" profile_user_2="{profile_user_2}" />
+
     <div class="flex flex-col sm:flex-row justify-between items-center my-10">
             <div class="profile flex flex-col w-full sm:w-auto">
                 <div class="flex items-center sm:justify-end sm:mr-5 mb-2 ">
@@ -115,37 +27,35 @@
                     <a href="https://www.root-me.org/{profile_user_1.nickname}" target="_blank" class="text-xl font-bold text-indigo-600 sm:text-right ">{profile_user_1.nickname}</a>
                 </div>
                 <div class="flex w-full sm:w-auto sm:flex-row-reverse bg-gradient-to-l from-indigo-800 top-indigo-900 shadow-md shadow-indigo-700/40 border border-indigo-600/60 rounded-full">
-                        <a href="https://www.root-me.org/{profile_user_1.nickname}" target="_blank" class="flex flex-col items-center">
-                            {#if parseInt(profile_user_1.stat.score) > parseInt(profile_user_2.stat.score) }
-                                <img src="./crown.png" alt="" class="w-8 -mt-4 -mb-4 z-90">
-                                <img src="https://root-me.org/{profile_user_1.logo}" alt="{profile_user_1.nickname}" title="{profile_user_1.nickname}" class="w-16 h-16 rounded-full border-4 border-yellow-500 shadow-lg shadow-yellow-500">
-                            {:else}
-                                <img src="https://root-me.org/{profile_user_1.logo}" alt="{profile_user_1.nickname}" title="{profile_user_1.nickname}" class="w-16 h-16 rounded-full border-4 border-indigo-500 shadow-lg shadow-indigo-500">
-                            {/if}
-                        </a>
-                        <div class="flex items-center px-5">
-                            <img src="https://www.root-me.org/squelettes/img/valid.svg" alt="Rank" class="w-10 mr-2">
-                            <div class="flex flex-col">
-                                <p class="font-bold text-yellow-500">{profile_user_1.stat.score}</p>
-                                <span class="text-xs italic text-yellow-300">Score</span>
-                            </div>
+                    <a href="https://www.root-me.org/{profile_user_1.nickname}" target="_blank" class="flex flex-col items-center">
+                        {#if parseInt(profile_user_1.stat.score) > parseInt(profile_user_2.stat.score) }
+                            <img src="./crown.png" alt="" class="w-8 -mt-4 -mb-4 z-90">
+                            <img src="https://root-me.org/{profile_user_1.logo}" alt="{profile_user_1.nickname}" title="{profile_user_1.nickname}" class="w-16 h-16 rounded-full border-4 border-yellow-500 shadow-lg shadow-yellow-500">
+                        {:else}
+                            <img src="https://root-me.org/{profile_user_1.logo}" alt="{profile_user_1.nickname}" title="{profile_user_1.nickname}" class="w-16 h-16 rounded-full border-4 border-indigo-500 shadow-lg shadow-indigo-500">
+                        {/if}
+                    </a>
+                    <div class="flex items-center px-5">
+                        <img src="https://www.root-me.org/squelettes/img/valid.svg" alt="Rank" class="w-10 mr-2">
+                        <div class="flex flex-col">
+                            <p class="font-bold text-yellow-500">{profile_user_1.stat.score}</p>
+                            <span class="text-xs italic text-yellow-300">Score</span>
                         </div>
-
-                        <div class="flex items-center px-5">
-                            <img src="https://www.root-me.org/squelettes/img/classement.svg" alt="Rank" class="w-10 mr-2">
-                            <div class="flex flex-col">
-                                <p class="font-bold text-yellow-500">{profile_user_1.stat.rank}</p>
-                                <span class="text-xs italic text-yellow-300">Rank</span>
-                            </div>
+                    </div>
+                    <div class="flex items-center px-5">
+                        <img src="https://www.root-me.org/squelettes/img/classement.svg" alt="Rank" class="w-10 mr-2">
+                        <div class="flex flex-col">
+                            <p class="font-bold text-yellow-500">{profile_user_1.stat.rank}</p>
+                            <span class="text-xs italic text-yellow-300">Rank</span>
                         </div>
-
-                        <div class="hidden xl:flex items-center px-5">
-                            <img src=" https://www.root-me.org/IMG/logo/rubon5.svg" alt="Rank" class="w-10 mr-2">
-                            <div class="flex flex-col">
-                                <p class="font-bold text-yellow-500">{profile_user_1.stat.chall_done}</p>
-                                <span class="text-xs italic text-yellow-300">Challenges</span>
-                            </div>
+                    </div>
+                    <div class="hidden xl:flex items-center px-5">
+                        <img src=" https://www.root-me.org/IMG/logo/rubon5.svg" alt="Rank" class="w-10 mr-2">
+                        <div class="flex flex-col">
+                            <p class="font-bold text-yellow-500">{profile_user_1.stat.chall_done}</p>
+                            <span class="text-xs italic text-yellow-300">Challenges</span>
                         </div>
+                    </div>
                 </div>
             </div>
 
